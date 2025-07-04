cf7de7eebaa096b88fe5bfd85c6aa1355e8e801e
Jozef Lumaj <jozef.lumaj@gmail.com>
2025-07-04 18:48:55 -0400

    init

--- src/macros.rs
+++ src/macros.rs
@@ -1,1 +1,13 @@
 // route function calls 
+
+use axum::{
+    routing::get,
+    Router,
+    response{
+        Html
+    }
+};
+
+pub async fn four_o_four() -> Html<&'static str> {
+    Html<&'static str>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-// mod routes;
-// routes::handle_request(req);
-// #[tokio::main]
-//async fn main() {
-//    println!("Hello, world!");
-// 
-// }
+mod routes;
+use routes::four_o_four;
+use axum::{
+    routing::get,
+    Router
+};
 
-fn main() {
-    println!("Hello World")
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(four_o_four));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
