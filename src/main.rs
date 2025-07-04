bf9a60fb58866401e14282320fd5666ca7333753
Jozef Lumaj <jozef.lumaj@gmail.com>
2025-07-04 18:53:41 -0400

    init

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,6 @@ async fn main() {
     let app = Router::new().route("/", get(four_o_four));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
