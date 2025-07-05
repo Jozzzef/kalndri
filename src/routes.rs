8a1e4c56f028c68c0e32e9325f01d6aa609ead00
Jozef Lumaj <jozef.lumaj@gmail.com>
2025-07-04 21:51:22 -0400

    init

--- src/routes.rs
+++ src/routes.rs
@@ -9,17 +9,48 @@ use handlebars::Handlebars;
 use serde::Serialize;
 
 #[derive(Serialize)]
-struct Person { 
-    name: String, 
-    age: u32 
+struct ErrorObject { 
+    status_code: i16 
+}
+
+use std::env;
+
+fn retrieve_htmx(relative_file_path: &str) -> Result<String, StatusCode> {
+
+    let current_dir = match env::current_dir(){
+        Ok(val) => {val},
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    };// This returns PathBuf
+    let full_path = current_dir.join(relative_file_path); // PathBuf::join()
+    println!("{:?}", &full_path);
+
+    let mut hbs = Handlebars::new();
+    match hbs.register_template_file("error_htmx", &full_path){
+        Ok(value) => println!("Result: {:?}", value),
+        Err(error) => {
+            println!("{:?}", &full_path);
+            eprintln!("Error: {}", error);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+    println!("{:?}", &hbs);
+    let err_obj = ErrorObject { status_code: 404 };
+    match hbs.render("error_htmx", &err_obj){
+        Ok(value) => {
+            println!("Result: {:?}", value);
+            Ok(value)
+        },
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub async fn four_o_four() -> Result<Html<String>, StatusCode> {
-    let mut hbs = Handlebars::new();
-    hbs.register_template_file("template", "path/to/template.hbs")
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let person = Person { name: "Alice".to_string(), age: 30 };
-    let rendered = hbs.render("template", &person)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok(Html(rendered))
+    Ok(Html(retrieve_htmx("src/templates/error.hbs")?))
+    //Ok(Html("<div>wtf</div>".to_string()))
 }
